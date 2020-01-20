ad67b2c3b828f1111a04b76b4ed28420dcfea793
devandroid-2020 <60059209+devandroid-2020@users.noreply.github.com>
2020-01-21 01:07:51 +0530

    UI Implementation - Gradle, layout, dimens, strings, styles implemented

--- app/src/main/java/com/converter/numbertoword/MainActivity.kt
+++ app/src/main/java/com/converter/numbertoword/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.converter.numbertoword
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
 import com.converter.numbertoword.contract.ConverterContract
+
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), ConverterContract.View {
     override fun displayWord(convertedWord: String?) {
@@ -12,5 +15,12 @@ class MainActivity : AppCompatActivity(), ConverterContract.View {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
     }
+
 }
