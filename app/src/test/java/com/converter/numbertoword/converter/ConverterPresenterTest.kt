271396ade3b4188bd35bc466426490b6b1c5c4f7
devandroid-2020 <60059209+devandroid-2020@users.noreply.github.com>
2020-01-20 22:40:51 +0530

    Mockito Unit Test Cases: Dependency added in gradle and unit test class ConverterPresenterTest created

---
+++ app/src/test/java/com/converter/numbertoword/converter/ConverterPresenterTest.kt
@@ -0,0 +1,25 @@
+package com.converter.numbertoword.converter
+
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class ConverterPresenterTest {
+
+    @Before
+    fun setUp() {
+
+    }
+
+    @Test
+    fun testConvertNumberToWordSuccess() {
+
+    }
+
+    @Test
+    fun testConvertNumberToWordFail() {
+
+    }
+}
